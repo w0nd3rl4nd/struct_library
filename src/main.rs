11f1d8bbd85210bbd87e1e78866aabc8eb22f68e
w0nd3rl4nd <alicealiagamoreno@gmail.com>
2025-06-04 09:48:08 +0200

    Add simple menu structure

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-#[derive(Debug)]
+use std::{io};
+
 struct Book {
     title: String,
     author: String,
@@ -43,7 +44,42 @@ impl Book {
 }
 
 fn main() {
+    let mut library: Vec<Book> = Vec::new();
+    let mut choice: u8 = 0;
+
     loop {
-        println!("Choose an option!");
+        choice = menu();
+        match choice {
+            1=>println!("Add book "),
+            2=>println!("Show all books"),
+            3=>println!("Borrow book"),
+            4=>println!("Return book"),
+            _=>println!("Wildcard pressed")
+        }
+    }
+}
+
+fn menu () -> u8 {
+    println!("Choose an option!");
+    println!("1. Add new book");
+    println!("2. View all books");
+    println!("3. Borrow book");
+    println!("4. Return book");
+    println!("5. Exit library");
+
+    let mut input = String::new();
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line"); 
+
+    return parse_string(&input);
+}
+
+fn parse_string(s: &str) -> u8 {
+
+    match s.trim().parse::<u8>() {
+        Ok(n) => return n,
+        Err(e) => panic!("{}", e),
     }
 }
